8478065f991275dec95b103eab64cf6617c8cc0b
Akinori Sakakibara <gorion.1213@gmail.com>
2020-10-15 01:08:26 +0900

    Created the footer component #3

---
+++ components/AppFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer absolute>
+    <v-row justify="center">
+      <v-btn v-for="link in links" :key="link" color="black" text rounded class="my-2">{{ link }}</v-btn>
+      <v-col class=" py-2 text-center black--text" cols="12">
+        &copy; 2020 Akinori Sakakibara. All Right Reserved.
+      </v-col>
+    </v-row>
+    <!-- <div class="footer"> -->
+    <!-- </div> -->
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      'Cleanerとは',
+      'クリーナーをみる',
+      'プロジェクトをみる',
+      'イベントをみる',
+      'お問い合わせ',
+    ],
+  }),
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <v-app>
     <AppHeader />
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
