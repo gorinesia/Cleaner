76a07a97a636e5b23c7d87a2271b8b9b50616d75
Akinori Sakakibara <gorion.1213@gmail.com>
2020-10-17 22:10:17 +0900

    Created the Auth directory

---
+++ pages/Auth/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <p style="color: #00ACC1">ログアウトしました</p>
+      <p>ログインはこちら</p>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'logout',
+  data: () => ({
+    showPassword: false
+  })
+};
+</script>
+
+<style>
+
+</style>
