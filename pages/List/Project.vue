4fb6611ecc211e611eac21dee017543a8c9e3d55
Akinori Sakakibara <gorion.1213@gmail.com>
2020-10-21 14:41:39 +0900

    Setupped the firebase

--- pages/List/Project.vue
+++ pages/List/Project.vue
@@ -1,100 +1,86 @@
 <template>
   <v-app>
-  <h2 style=" color: #00ACC1;">イベントをみる</h2>
     <v-container>
       <v-row>
-        <v-col cols="6">
-          <v-card class="container">
-            <div class="row">
-              <div class="col-md-4 my-3">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">東京を綺麗にしましょう</h4>
-                    <h6 class="card-subtitle mb-2 text-muted">test articke1</h6>
-                    <p class="card-text">
-                      test article1, test article1, test article1, test article1, test article1
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card class="container">
-            <div class="row">
-              <div class="col-md-4 my-3">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">渋谷を片付けましょう</h4>
-                    <h6 class="card-subtitle mb-2 text-muted">test articke1</h6>
-                    <p class="card-text">
-                      test article1, test article1, test article1, test article1, test article1
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
+        <v-col cols="2">
           <v-card class="mx-auto" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="overline mb-1">10月18日</div>
-                <div class="headline mb-3" style="color: #00ACC1;">東京を綺麗にしましょう。</div>
-                <p class="mb-5">僕たちにできることを。人もゴミも多く、やりがいのある場所をみんなで綺麗にしませんか？</p>
-                <p class="font-weight-bold">日時： 10月3日  9:00~12:00</p>
-                <p class="font-weight-bold">場所： 東京都渋谷区</p>
-              </v-list-item-content>
-
-              <v-avatar tile size="200" color="cyan" :src="image_src" class="ml-5">
-                <img :src="image_src" alt="">
-              </v-avatar>
-            </v-list-item>
-
-            <v-container fluid>
-              <v-row justify="center">
-
-                <v-expansion-panels popout>
-                  <v-expansion-panel v-for="(message, i) in messages" :key="i" hide-actions>
-                    <v-expansion-panel-header>
-                      <v-row align="center" class="spacer" no-gutters>
-                        <v-col cols="4" sm="2" md="1">
-                          <v-avatar size="36px">
-                            <img v-if="message.avatar" :src="image_src" alt="">
-                            <v-icon v-else :color="message.color" v-text="message.icon"></v-icon>
-                          </v-avatar>
-                        </v-col>
-
-                        <v-col class="hidden-xs-only" sm="5" md="3">
-                          <strong v-html="message.name"></strong>
-                          <span v-if="message.total" class="grey--text">
-                            &nbsp;({{ message.total }})
-                          </span>
-                        </v-col>
-
-                        <v-col class="text-no-wrap" cols="5" sm="3">
-                          <v-chip v-if="message.new" :color="`${message.color} lighten-4`" class="ml-0 mr-2 black--text" label small>
-                            {{ message.new }} new
-                          </v-chip>
-                          <strong v-html="message.title"></strong>
-                        </v-col>
-
-                        <v-col v-if="message.excerpt" class="grey--text text-truncate hidden-sm-and-down">
-                          &mdash;
-                          {{ message.excerpt }}
-                        </v-col>
-                      </v-row>
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                      <v-divider></v-divider>
-                      <v-card-text></v-card-text>
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-              </v-row>
-            </v-container>
+            <v-row align="end" class="fill-height">
+              <v-col align-self="start"  cols="12">
+                <v-avatar class="profile" color="grey" size="164">
+                  <v-img :src="image_src"></v-img>
+                </v-avatar>
+              </v-col>
+              <v-col class="py-0">
+                <v-list-item color="rgba(0, 0, 0, .4)">
+                  <v-list-item-content>
+                    <v-list-item-title class="font-weight-bold title" style="color: #00ACC1">
+                      AKI
+                    </v-list-item-title>
+                    <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="10">
+          <h2 style="color: #00ACC1; margin-bottom: 10px">プロジェクトをみる</h2>
+          <v-card class="mb-5">
+            <v-row>
+              <v-col cols="2">
+                <v-col align-self="start"  cols="12">
+                <v-avatar tile size="120" color="cyan" :src="image_src" class="ml-5">
+                  <img :src="image_src" alt="">
+                </v-avatar>
+                </v-col>
+              </v-col>
+              <v-col cols="10">
+                <v-avatar class="profile" color="grey" size="80">
+                  <v-img :src="image_src"></v-img>
+                </v-avatar>
+                    <span class="headline mb-3 font-weight-bold" style="color: #00ACC1;" @click="$router.push('/personal/personalproject')">John</span>
+                    <span class="overline mb-1 float-right grey--text">10分前</span>
+                    <p class="ma-5">会社の近くの神社です。</p>
+              </v-col>
+            </v-row>
+          </v-card>
+          <v-card class="mb-5">
+            <v-row>
+              <v-col cols="2">
+                <v-col align-self="start"  cols="12">
+                <v-avatar tile size="120" color="cyan" :src="image_src" class="ml-5">
+                  <img :src="image_src" alt="">
+                </v-avatar>
+                </v-col>
+              </v-col>
+              <v-col cols="10">
+                <v-avatar class="profile" color="grey" size="80">
+                  <v-img :src="image_src"></v-img>
+                </v-avatar>
+                    <span class="headline mb-3 font-weight-bold" style="color: #00ACC1;">John</span>
+                    <span class="overline mb-1 float-right grey--text">10分前</span>
+                    <p class="ma-5">会社の近くの神社です。</p>
+              </v-col>
+            </v-row>
+          </v-card>
+          <v-card class="mb-5">
+            <v-row>
+              <v-col cols="2">
+                <v-col align-self="start"  cols="12">
+                <v-avatar tile size="120" color="cyan" :src="image_src" class="ml-5">
+                  <img :src="image_src" alt="">
+                </v-avatar>
+                </v-col>
+              </v-col>
+              <v-col cols="10">
+                <v-avatar class="profile" color="grey" size="80">
+                  <v-img :src="image_src"></v-img>
+                </v-avatar>
+                    <span class="headline mb-3 font-weight-bold" style="color: #00ACC1;">John</span>
+                    <span class="overline mb-1 float-right grey--text">10分前</span>
+                    <p class="ma-5">会社の近くの神社です。</p>
+              </v-col>
+            </v-row>
           </v-card>
         </v-col>
       </v-row>
