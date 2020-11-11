36cd9cec0777af8ba8e7dfa4fe3c5c8aa3495db6
Akinori Sakakibara <gorion.1213@gmail.com>
2020-11-11 21:21:17 +0900

    Changed environment file at nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,5 +99,13 @@ export default {
   */
   build: {
     transpile: [/^vue2-google-maps($|\/)/]
+  },
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DB_URL: process.env.FIREBASE_DB_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
   }
 }
