0a1af29ea65313f5fb0f31e0e444797ee41c1851
Akinori Sakakibara <gorion.1213@gmail.com>
2020-11-13 00:12:42 +0900

    Changed config file at firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,16 +3,18 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
+const config = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECTID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+}
+
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    databaseURL: process.env.FB_DATABASE_URL,
-    projectId: process.env.FB_PROJECTID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
-  })
+  firebase.initializeApp(config)
 }
 
 export default firebase;
