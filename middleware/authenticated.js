8a82895870cab586daccb961a257d3c402ce0d3f
Akinori Sakakibara <gorion.1213@gmail.com>
2020-11-14 01:33:23 +0900

    Changed the env file for heroku

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 export default function ({ store, redirect }) {
   firebase.auth().onAuthStateChanged((user) => {
