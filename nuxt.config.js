ee4f85e60139c0ce164939e35743600bf910cfb0
Akinori Sakakibara <gorion.1213@gmail.com>
2020-11-14 05:56:13 +0900

    Tried to check another env file for heroku deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,12 +104,12 @@ export default {
     transpile: [/^vue2-google-maps($|\/)/]
   },
   env: {
-    VUE_APP_FB_API_KEY: process.env.FB_API_KEY,
-    VUE_APP_FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
-    VUE_APP_FB_DATABASE_URL: process.env.FB_DATABASE_URL,
-    VUE_APP_FB_PROJECT_ID: process.env.FB_PROJECT_ID,
-    VUE_APP_FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
-    VUE_APP_FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
-    VUE_APP_FB_APP_ID: process.env.FB_APP_ID
+    VUE_APP_FB_API_KEY: process.env.VUE_APP_FB_API_KEY,
+    VUE_APP_FB_AUTH_DOMAIN: process.env.VUE_APP_FB_AUTH_DOMAIN,
+    VUE_APP_FB_DATABASE_URL: process.env.VUE_APP_FB_DATABASE_URL,
+    VUE_APP_FB_PROJECT_ID: process.env.VUE_APP_FB_PROJECT_ID,
+    VUE_APP_FB_STORAGE_BUCKET: process.env.VUE_APP_FB_STORAGE_BUCKET,
+    VUE_APP_FB_MESSAGING_SENDER_ID: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+    VUE_APP_FB_APP_ID: process.env.VUE_APP_FB_APP_ID
   }
 }
