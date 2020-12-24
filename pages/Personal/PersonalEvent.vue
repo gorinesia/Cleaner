dd5716e2b991b87798cb77aca123095dfbf1e019
Akinori Sakakibara <gorion.1213@gmail.com>
2020-12-24 23:14:40 +0900

    Installed the express & stripe to make chacking system

--- pages/Personal/PersonalEvent.vue
+++ pages/Personal/PersonalEvent.vue
@@ -188,7 +188,7 @@ export default {
     getProfile(id) {
       console.log(id);
     },
-    pay() {
+    async pay() {
       const Stripe = require('stripe');
       const stripe = Stripe('pk_test_51Hp8W6GM8QHm52Sew543CT6L0qkt1A4K6eKS89CRiVKKrLCHdzSaAEsmseYVrYJdDx3x0MWjt3kIiShsjOlo73w800iAHOtu3v');
 
