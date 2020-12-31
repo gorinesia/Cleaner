874ca67bf8fc6f6ad2f29665bce1d187822fd03d
Akinori Sakakibara <gorion.1213@gmail.com>
2020-12-31 23:44:04 +0900

    Created the page of register for the customer

---
+++ pages/Customer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <layout>
+      <main>
+        <div>
+          <h4>こちらからクレジットカードを登録してください</h4>
+          <p>**テスト用の番号 "4242424242424242" を使用してください**</p>
+        </div>
+        <div>
+          <h4>お客様のお名前を登録してください</h4>
+          <form action="">
+            <input type="text">
+            <button>名前を登録する</button>
+          </form>
+        </div>
+      </main>
+    </layout>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
