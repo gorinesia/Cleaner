dc552352ec94f112d213fb200bdefbd030308865
Akinori Sakakibara <gorion.1213@gmail.com>
2021-01-21 22:50:55 +0900

    Implementing the api for register the customer

--- pages/Customer.vue
+++ pages/Customer.vue
@@ -10,7 +10,7 @@
           <h4>お客様のお名前を登録してください</h4>
           <form action="">
             <input type="text">
-            <button>名前を登録する</button>
+            <v-btn>名前を登録する</v-btn>
           </form>
         </div>
       </main>
@@ -22,7 +22,26 @@
 import { POST } from '@nuxtjs/axios';
 
 export default {
-
+  mounted() {
+    this.stripe = Stripe('pk_test_51Hp8W6GM8QHm52Sew543CT6L0qkt1A4K6eKS89CRiVKKrLCHdzSaAEsmseYVrYJdDx3x0MWjt3kIiShsjOlo73w800iAHOtu3v');
+    let elements = this.stripe.elements();
+    this.card = elements.create('card');
+    this.card.mount(this.$refs.card);
+  },
+  methods: {
+    async registerCustomer(e) {
+      e.preventDefault();
+      const result = this.$axios.$post('/api/register', {
+        customerName: name
+      });
+      console.log(result);
+      customerSetter({
+        name: result.name,
+        id: result.id,
+        client_secret: result.client_secret
+      })
+    }
+  }
 }
 </script>
 
