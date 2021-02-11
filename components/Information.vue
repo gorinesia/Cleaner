e3aff2157d085097acd06081c31fc06b57010615
Akinori Sakakibara <gorion.1213@gmail.com>
2021-02-11 22:28:17 +0900

    Changed the margin of v-tabs on top-page

--- components/Information.vue
+++ components/Information.vue
@@ -4,23 +4,19 @@
       <v-col cols="12" md="4" align="center" justify="center">
         <div class="font-weight-bold">誰でもできる</div>
         <img src="~/assets/img/lonly.jpg">
-        <p>あなた自身が<span class="cyan--text text--darken-1 font-weight-bold">クリーナー</span>(掃除屋)となり、ゴミ拾いを<span class="orange--text font-weight-bold">プロジェクト</span>として、はじめることができます。<br>
-        マイページで日々のプロジェクトを気軽に投稿
-        できます。</p>
+        <p>あなた自身が<span class="cyan--text text--darken-1 font-weight-bold">クリーナー</span>(掃除屋)となり、ゴミ拾いを<span class="orange--text font-weight-bold">プロジェクト</span>としてはじめることができます。マイページで日々のプロジェクトを気軽に投稿できます。</p>
       </v-col>
       <v-divider vertical></v-divider>
       <v-col cols="12" md="4" align="center" justify="center">
         <div class="font-weight-bold">みんなでできる</div>
         <img src="~/assets/img/people.jpg">
-        <p><span class="orange--text font-weight-bold">イベント</span>を企画して、自分のプロジェクトに仲間を募ったり、誰かのプロジェクトに参加することもできます。</p>
+        <p><span class="orange--text font-weight-bold">イベント</span>を企画し、自分のプロジェクトに仲間を募ったり、誰かのプロジェクトに参加することもできます。</p>
       </v-col>
       <v-divider vertical></v-divider>
       <v-col cols="12" md="3" align="center" justify="center">
         <div class="font-weight-bold">誰かをサポートできる</div>
         <img src="~/assets/img/support.jpg">
-        <p>あなたのプロフィールページやあなたの企画したプロジェクトに対して、
-        ”あなたのその行動には価値がある。”
-        そう思ってくれた人は、あなたに対して金銭的な<span class="orange--text font-weight-bold">サポート</span>を施すことができます。</p>
+        <p>誰かの日々のプロジェクトや企画したイベントに対して、「お疲れさま」の意味を込めて、金銭的な<span class="orange--text font-weight-bold">サポート</span>を施すことができます。</p>
       </v-col>
     </v-row>
   </v-container>
@@ -31,5 +27,9 @@ img {
   width: 100px;
   height: 100px;
   border-radius: 50%;
+}
+
+p {
+  white-space: pre-wrap;
 }
 </style>
