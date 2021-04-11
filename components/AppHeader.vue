2a9300bf9c1ac093a2c205ed97f2f064388a1eed
Akinori Sakakibara <gorion.1213@gmail.com>
2021-04-11 23:45:18 +0900

    Modified the margin of header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,98 +2,179 @@
   <header>
     <v-container my-n3 fluid>
       <v-row>
-        <v-app-bar color="white" style="color: #00ACC1" class="font-weight-bold">
-            <v-toolbar-title>
-              <span><v-icon>fas fa-lock</v-icon></span>
-              <nuxt-link v-if="!loggedIn" class="app-title font-weight-bold text-h4 cyan--text text--darken-1 text-decoration-none" to="/">
-                <v-icon large color="cyan darken-1">mdi-earth</v-icon>
-                Cleaner
-              </nuxt-link>
-              <nuxt-link  v-else class="app-title font-weight-bold text-h4 cyan--text text--darken-1 text-decoration-none" to="/mypage">
-                <v-icon large color="cyan darken-1">mdi-earth</v-icon>
-                Cleaner
-              </nuxt-link>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <nav>
-              <v-tabs>
-                <nav v-if="!loggedIn" class="mt-1">
-                  <button class="btn--guest btn--guest--radius" @click="guestLogin">ゲストログイン</button>
-                  <v-btn rounded outlined color="#00ACC1" class="font-weight-bold" dark to="/auth/login">ログイン</v-btn>
-                  <v-btn rounded color="#00ACC1" class="font-weight-bold" dark to="/auth/signup">新規登録</v-btn>
-                </nav>
-                <nav v-else>
-                  <template>
-                    <div v-for="currentUser in
-                    currentUser" :key="currentUser.id">
-                      <v-menu offset-y>
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-img
-                              :src="currentUser.image"
-                              v-bind="attrs"
-                              v-on="on"
-                              width="48px"
-                              height="48px"
-                              style="border-radius: 50%; cursor: pointer; float: right"
-                            ></v-img>
-                        </template>
-                        <v-list>
-                          <v-list-item
-                            v-for="item in items"
-                            :key="item.title" :to="item.link"
-                          >
+        <v-app-bar
+          color="white"
+          style="color: #00acc1"
+          class="font-weight-bold"
+        >
+          <v-toolbar-title>
+            <span><v-icon>fas fa-lock</v-icon></span>
+            <nuxt-link
+              v-if="!loggedIn"
+              class="app-title font-weight-bold text-h4 cyan--text text--darken-1 text-decoration-none"
+              to="/"
+            >
+              <v-icon large color="cyan darken-1">mdi-earth</v-icon>
+              Cleaner
+            </nuxt-link>
+            <nuxt-link
+              v-else
+              class="app-title font-weight-bold text-h4 cyan--text text--darken-1 text-decoration-none"
+              to="/mypage"
+            >
+              <v-icon large color="cyan darken-1">mdi-earth</v-icon>
+              Cleaner
+            </nuxt-link>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <nav>
+            <v-tabs>
+              <nav v-if="!loggedIn" class="mt-1">
+                <button
+                  class="btn--guest btn--guest--radius"
+                  @click="guestLogin"
+                >
+                  ゲストログイン
+                </button>
+                <v-btn
+                  rounded
+                  outlined
+                  color="#00ACC1"
+                  class="font-weight-bold"
+                  dark
+                  to="/auth/login"
+                  >ログイン</v-btn
+                >
+                <v-btn
+                  rounded
+                  color="#00ACC1"
+                  class="font-weight-bold"
+                  dark
+                  to="/auth/signup"
+                  >新規登録</v-btn
+                >
+              </nav>
+              <nav v-else>
+                <template>
+                  <div v-for="currentUser in currentUser" :key="currentUser.id">
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-img
+                          :src="currentUser.image"
+                          v-bind="attrs"
+                          v-on="on"
+                          width="48px"
+                          height="48px"
+                          style="
+                            border-radius: 50%;
+                            cursor: pointer;
+                            float: right;
+                          "
+                        ></v-img>
+                      </template>
+                      <v-list>
+                        <v-list-item
+                          v-for="item in items"
+                          :key="item.title"
+                          :to="item.link"
+                        >
                           <v-list-item-icon>
                             <v-icon>{{ item.icon }}</v-icon>
                           </v-list-item-icon>
-                            <v-list-item-title>{{ item.title }}</v-list-item-title>
-                          </v-list-item>
-                          <v-btn rounded block color="#00ACC1" class="font-weight-bold mt-5" dark @click="logOut">ログアウト</v-btn>
-                        </v-list>
-                      </v-menu>
-                    </div>
-                  </template>
-                </nav>
-              </v-tabs>
-            </nav>
-          <v-app-bar-nav-icon color="#00ACC1" @click="drawer = true"></v-app-bar-nav-icon>
+                          <v-list-item-title>{{
+                            item.title
+                          }}</v-list-item-title>
+                        </v-list-item>
+                        <v-btn
+                          rounded
+                          block
+                          color="#00ACC1"
+                          class="font-weight-bold mt-5"
+                          dark
+                          @click="logOut"
+                          >ログアウト</v-btn
+                        >
+                      </v-list>
+                    </v-menu>
+                  </div>
+                </template>
+              </nav>
+            </v-tabs>
+          </nav>
+          <v-app-bar-nav-icon
+            color="#00ACC1"
+            @click="drawer = true"
+          ></v-app-bar-nav-icon>
         </v-app-bar>
       </v-row>
     </v-container>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list nav dense>
         <v-list-item-group>
           <v-list-item>
             <nav v-if="!loggedIn">
               <ul style="list-style: none">
                 <li class="ma-3">
-                  <v-btn rounded color="#1A237E" dark class="font-weight-bold orange--text text--darken-1" @click="guestLogin">ゲストログイン</v-btn>
+                  <v-btn
+                    rounded
+                    color="#1A237E"
+                    dark
+                    class="font-weight-bold orange--text text--darken-1"
+                    @click="guestLogin"
+                    >ゲストログイン</v-btn
+                  >
                 </li>
                 <li class="ma-4">
-                  <v-btn rounded block outlined color="#00ACC1" class="font-weight-bold" dark to="/auth/login">ログイン</v-btn>
+                  <v-btn
+                    rounded
+                    block
+                    outlined
+                    color="#00ACC1"
+                    class="font-weight-bold"
+                    dark
+                    to="/auth/login"
+                    >ログイン</v-btn
+                  >
                 </li>
                 <li class="ma-3">
-                  <v-btn rounded block color="#00ACC1" class="font-weight-bold" dark to="/auth/signup">新規登録</v-btn>
+                  <v-btn
+                    rounded
+                    block
+                    color="#00ACC1"
+                    class="font-weight-bold"
+                    dark
+                    to="/auth/signup"
+                    >新規登録</v-btn
+                  >
                 </li>
               </ul>
             </nav>
             <nav v-else>
-              <v-card class="pa-2" height="250px" width="230" v-for="currentUser in currentUser" :key="currentUser.id">
+              <v-card
+                class="pa-2"
+                height="250px"
+                width="230"
+                v-for="currentUser in currentUser"
+                :key="currentUser.id"
+              >
                 <v-navigation-drawer permanent>
                   <template v-slot:prepend>
                     <v-list-item two-line>
                       <v-list-item-avatar>
-                        <v-img :src="currentUser.image" width="30px" height="30px"></v-img>
+                        <v-img
+                          :src="currentUser.image"
+                          width="30px"
+                          height="30px"
+                        ></v-img>
                       </v-list-item-avatar>
 
                       <v-list-item-content>
-                        <nuxt-link class="cyan--text text--darken-1 font-weight-bold text-h5 text-decoration-none" to="/personal/profile" @click.native="getProfile(currentUser.id)">{{ currentUser.displayName }}</nuxt-link>
+                        <nuxt-link
+                          class="cyan--text text--darken-1 font-weight-bold text-h5 text-decoration-none"
+                          to="/personal/profile"
+                          @click.native="getProfile(currentUser.id)"
+                          >{{ currentUser.displayName }}</nuxt-link
+                        >
                       </v-list-item-content>
                     </v-list-item>
                   </template>
@@ -101,7 +182,11 @@
                   <v-divider></v-divider>
 
                   <v-list dense>
-                    <v-list-item v-for="item  in items" :key="item.title" :to="item.link">
+                    <v-list-item
+                      v-for="item in items"
+                      :key="item.title"
+                      :to="item.link"
+                    >
                       <v-list-item-icon>
                         <v-icon>{{ item.icon }}</v-icon>
                       </v-list-item-icon>
@@ -113,7 +198,15 @@
                   </v-list>
                 </v-navigation-drawer>
               </v-card>
-              <v-btn rounded block color="#00ACC1" class="font-weight-bold mt-5" dark @click="logOut">ログアウト</v-btn>
+              <v-btn
+                rounded
+                block
+                color="#00ACC1"
+                class="font-weight-bold mt-5"
+                dark
+                @click="logOut"
+                >ログアウト</v-btn
+              >
             </nav>
           </v-list-item>
         </v-list-item-group>
@@ -124,56 +217,56 @@
 
 <script>
 export default {
-  name: 'app-header',
+  name: "app-header",
   data() {
     return {
       drawer: false,
-      email: 'test@test.com',
-      password: '123456',
+      email: "test@test.com",
+      password: "123456",
       items: [
         {
-          title: 'ユーザー情報設定',
-          icon: 'mdi-folder',
-          link: '/edit/profile'
+          title: "ユーザー情報設定",
+          icon: "mdi-folder",
+          link: "/edit/profile",
         },
         {
-          title: 'メッセージ管理',
-          icon: 'mdi-account-multiple',
-          link: '/edit/message'
+          title: "メッセージ管理",
+          icon: "mdi-account-multiple",
+          link: "/edit/message",
         },
         {
-          title: 'サポート管理',
-          icon: 'mdi-upload',
-          link: '/edit/support'
+          title: "サポート管理",
+          icon: "mdi-upload",
+          link: "/edit/support",
         },
       ],
-      isOpen: false
-    }
+      isOpen: false,
+    };
   },
-  computed : {
+  computed: {
     loggedIn() {
-      return this.$store.getters['user/loggedIn']
+      return this.$store.getters["user/loggedIn"];
     },
     currentUser() {
-      return this.$store.getters['user/currentUser']
-    }
+      return this.$store.getters["user/currentUser"];
+    },
   },
   methods: {
     guestLogin() {
-      this.$store.dispatch('user/logInAction', {
+      this.$store.dispatch("user/logInAction", {
         email: this.email,
-        password: this.password
-      })
+        password: this.password,
+      });
     },
     logOut() {
-      this.$store.dispatch('user/logOutAction');
+      this.$store.dispatch("user/logOutAction");
     },
     open() {
-      console.log('right')
-      this.isOpen = !this.isOpen
-    }
-  }
-}
+      console.log("right");
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -193,12 +286,12 @@ export default {
   display: none;
 
   @include display_pc {
-    display: block !important
+    display: block !important;
   }
 }
 .btn--guest {
   color: #fff;
-  background-color: #00897B;
+  background-color: #00897b;
   opacity: 0.8;
 }
 
@@ -225,7 +318,7 @@ export default {
 .btn--logout {
   font-weight: bold;
   color: #fff;
-  background-color: #00ACC1;
+  background-color: #00acc1;
   padding: 8px;
   margin-bottom: -20px;
 }
@@ -237,76 +330,76 @@ nav {
   margin-right: 50px;
 }
 nav .v-btn {
-  margin-left: 20px;
+  margin-left: 15px;
 }
 #nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    max-width: 1280px;
-    margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
 }
 
 #nav h1 {
-    margin: 0 0 0 20px;
-    color: #fff;
+  margin: 0 0 0 20px;
+  color: #fff;
 }
 
 #nav > ul {
-    display: flex;
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
 }
 
 #nav > ul > li {
-    margin: 0 20px 0 0;
+  margin: 0 20px 0 0;
 }
 
 #nav > ul > li > a {
-    display: block;
-    height: auto;
-    padding: 20px;
-    color: #fff;
-    text-decoration: none;
+  display: block;
+  height: auto;
+  padding: 20px;
+  color: #fff;
+  text-decoration: none;
 }
 
 #nav > ul > li > span {
-    position: relative;
-    display: block;
-    height: auto;
-    padding: 20px;
-    color: #fff;
-    text-decoration: none;
+  position: relative;
+  display: block;
+  height: auto;
+  padding: 20px;
+  color: #fff;
+  text-decoration: none;
 }
 
 #nav > ul > li > span:after {
-    content: '▼';
-    display: inline-block;
-    transform: rotate(90deg);
+  content: "▼";
+  display: inline-block;
+  transform: rotate(90deg);
 }
 
 .dropdown {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    display: none;
-    padding: 0;
-    list-style-type: none;
-    background-color: #007db9;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  display: none;
+  padding: 0;
+  list-style-type: none;
+  background-color: #007db9;
 }
 
 .dropdown li {
-    width: 250px;
-    border-bottom: 1px solid #fff;
+  width: 250px;
+  border-bottom: 1px solid #fff;
 }
 
 .dropdown li a {
-    display: block;
-    padding: 10px;
-    color: #fff;
-    text-decoration: none;
+  display: block;
+  padding: 10px;
+  color: #fff;
+  text-decoration: none;
 }
 
 .isOpen {
