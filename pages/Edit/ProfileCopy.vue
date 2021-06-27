126ac05267aee92ac385ac06ba4aed92303b2239
Akinori Sakakibara <gorion.1213@gmail.com>
2021-06-27 22:19:34 +0900

    Trying to modify the profile page

--- pages/Edit/ProfileCopy.vue
+++ pages/Edit/ProfileCopy.vue
@@ -1,97 +1,174 @@
 <template>
-  <v-app class="mx-10">
-    <v-alert dense text type="success" :value="alert" style="position: fixed; z-index: 1; right: 200px; bottom: 100px">プロフィール情報を更新しました</v-alert>
-    <h2 class="cyan--text text--darken-1">ユーザー情報</h2>
-    <v-container max-width="600px">
-      <v-card v-for="currentUser in currentUser" :key="currentUser.id" class="pa-5" >
-        <label class="postImage-appendBtn block font-weight-bold">プロフィール画像</label>
-        <input class="d-block" type="file" id="btnUpload" @change="btnUploadChange" value="アップロード" data-label="画像の添付"><br>
-        <v-img :src="image" width="100" height="100"></v-img>
-        <label class="font-weight-bold">ユーザー名</label>
-        <v-text-field v-model="displayName" class="white" :placeholder="currentUser.displayName"></v-text-field>
-        <label class="font-weight-bold">プロフィール</label>
-        <v-textarea v-model="comment" class="white" placeholder="自己紹介を書きましょう"></v-textarea>
-        <v-btn @click="updateProfile(currentUser.id)" class="mt-8 float-right font-weight-bold" color="cyan" dark>更新</v-btn>
+  <v-app>
+    <v-container v-for="currentUser in currentUser" :key="currentUser.id">
+      <v-card outlined class="mb-10">
+        <v-row class="fill-height">
+          <v-col align-self="start" cols="12" md="2">
+            <v-avatar class="profile ml-3 mt-3" color="grey" size="80">
+              <v-img :src="currentUser.image"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col cols="12" md="10">
+            <v-list-item color="rgba(0, 0, 0, .4)">
+              <v-list-item-content>
+                <v-list-item-title
+                  class="cyan--text text--darken-1 font-weight-bold title mb-2"
+                  dark
+                >
+                  {{ currentUser.displayName }}
+                </v-list-item-title>
+                <p class="grey--text mb-2">{{ currentUser.place }}</p>
+                <p>{{ currentUser.comment }}</p>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-card>
+
+      <v-card>
+        <v-tabs
+          v-model="tabs"
+          background-color="cyan darken-1"
+          centered
+          dark
+          icons-and-text
+        >
+          <v-tab v-for="n in items" :key="n">
+            {{ n }}
+          </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tabs">
+          <v-tab-item>
+            <v-card>
+              <template>
+                <v-divider />
+                <v-hover v-slot="{ hover }">
+                  <v-card
+                    :class="{ 'on-hover': hover }"
+                    @click="getPersonalProjectId(article.id)"
+                  >
+                    <v-row>
+                      <v-col cols="3">
+                        <v-avatar tile size="100" class="ml-5">
+                          <v-img :src="currentUser.image"></v-img>
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" md="4">
+                        <v-avatar class="profile" color="grey" size="60">
+                          <v-img :src="currentUser.displayImage"></v-img>
+                        </v-avatar>
+                        <span
+                          class="headline mb-3 font-weight-bold"
+                          style="color: #00acc1"
+                          >{{ currentUser.displayName }}</span
+                        >
+                        <p class="my-5 font-weight-bold">
+                          {{ currentUser.comment }}
+                        </p>
+                      </v-col>
+                      <v-col cols="12" md="5">
+                        <span class="grey--text float-right">{{
+                          currentUser.date
+                        }}</span>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-hover>
+              </template>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card flat>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-1">1030日</div>
+                  <div class="headline mb-3 font-weight-bold">
+                    渋谷の路上でゴミを拾います。
+                  </div>
+                  <p class="mb-5">
+                    僕たちにできることを。人もゴミも多く、やりがいのある場所をみんなで綺麗にしませんか？
+                  </p>
+                  <p class="font-weight-bold">日時： 10月3日 9:00~12:00</p>
+                  <p class="font-weight-bold">場所： 東京都渋谷区</p>
+                </v-list-item-content>
+
+                <v-avatar
+                  tile
+                  size="200"
+                  color="cyan"
+                  :src="image_src"
+                  class="ml-5"
+                >
+                  <img :src="image_src" alt="" />
+                </v-avatar>
+              </v-list-item>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
       </v-card>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-
 export default {
-  layout: 'loggedIn',
+  name: "profile",
+  layout: "loggedIn",
+  middleware: "authenticated",
   data() {
     return {
-      displayName: '',
-      comment: '',
-      alert: false,
-      dialog: false
-    }
+      tabs: null,
+      items: ["プロジェクト", "イベント"],
+      image_src: require("@/assets/img/everyone.jpg"),
+    };
   },
   computed: {
     currentUser() {
-      return this.$store.getters['user/currentUser']
+      return this.$store.getters["user/currentUser"];
     },
     uid() {
-      return this.$store.getters['user/uid']
+      return this.$store.getters["user/uid"];
     },
-    image: {
-      get() {
-        return this.$store.getters['user/image']
-      },
-      set(value) {
-        this.$store.commit('user/setImage', value)
-      }
+    personalDatas() {
+      return this.$store.getters["user/personalDatas"];
+    },
+    articles() {
+      return this.$store.getters["project/articles"];
+    },
+    personalProjectId() {
+      return this.$store.getters["project/personalProjectId"];
     },
   },
   mounted() {
-    this.$store.dispatch('user/logInUserDisplay', {
+    this.$store.dispatch("user/logInUserDisplay", {
       uid: this.uid,
-      email: this.currentUser[0].email
     });
+    console.log(this.currentUser);
+    this.$store.dispatch("project/getMessage", {
+      uid: this.uid,
+    });
+    this.$store.dispatch("event/getEvent");
   },
   methods: {
-    btnUploadChange(ev) {
-      this.$store.dispatch('user/btnUploadChange', {
-        ev
-      });
-    },
-    getUrl(ev) {
-      this.$store.dispatch('user/getUrl', {
-        ev
-      });
-    },
-    updateProfile(id) {
-      console.log(this.currentUser);
-      this.$store.dispatch('user/updateProfile', {
+    getPersonalProjectId(id) {
+      this.$store.dispatch("project/getPersonalProjectId", {
         id,
-        uid: this.currentUser[0].uid,
-        displayName: this.displayName,
-        comment: this.comment,
-        image: this.image,
-        email: this.currentUser[0].email
       });
-      this.alert = true;
-      setTimeout(() => {
-        this.alert = false
-      }, 3000);
-      this.displayName = '';
-      this.place = '';
-      this.comment = '';
-      this.date = '';
     },
-    getUserProfile(uid) {
-      this.$store.dispatch('project/getUserProfile', {
-        uid
+    getPersonalEvent(id) {
+      this.$store.dispatch("event/getPersonalEvent", {
+        id,
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+main {
+  .v-main {
+    margin: 0 100px;
+  }
+}
 </style>
